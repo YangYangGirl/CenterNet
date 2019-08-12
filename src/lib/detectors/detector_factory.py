a964c5b5ce939f41db15285e258cf9839806cbf9
yangyanggirl <1525927685@qq.com>
2019-08-12 16:18:43 +0800

    add link part

--- src/lib/detectors/detector_factory.py
+++ src/lib/detectors/detector_factory.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from .plnres import PlnresDetector
 from .exdet import ExdetDetector
 from .ddd import DddDetector
 from .ctdet import CtdetDetector
 from .multi_pose import MultiPoseDetector
 
 detector_factory = {
+  'plnres': PlnresDetector,
   'exdet': ExdetDetector, 
   'ddd': DddDetector,
   'ctdet': CtdetDetector,
