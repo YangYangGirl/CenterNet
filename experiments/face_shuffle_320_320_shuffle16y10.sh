93db5c8d90798fdf15171698ae31ab075868dec7
huangxiaohu <huangxiaohu#hust.edu.cn>
2020-04-24 09:02:07 +0000

    clear env

---
+++ experiments/face_shuffle_320_320_shuffle16y10.sh
@@ -0,0 +1,12 @@
+var=shuffle_320_320_shuffle16y10
+var_arch=shuffle16y10
+cd ../src
+# train
+python main.py --task ctdet --dataset face  --num_epochs 300 --batch_size  128 --master_batch 128   --lr 1.25e-4 --gpus 1  --metric loss  --exp_id  $var  --arch $var_arch
+# produce output
+python test_widerface.py --arch $var_arch   --num_class 1 --dataset face --task ctdet --load_model "../exp/ctdet/"$var"/model_best.pth"  --outdir "../output/"$var"/"
+# evaluate output
+cd widerface_evaluate
+# multi scale test
+python3 evaluation.py -p "../../output/"$var"/" -g ./ground_truth
+cd ..
